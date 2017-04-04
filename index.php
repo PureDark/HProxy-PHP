0cfa2c855573847eb83603e30acd6586ae6a79f9
PureDark <bruce.lee.liang@gmail.com>
2017-04-04 11:33:25 +0800

    try update

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
 <?php
-	//定义编码  
     $post = ($_SERVER['REQUEST_METHOD']=='POST');
 	$headers = get_all_headers();
 	if(isset($headers["Origin-Url"])&&!empty($headers["Origin-Url"])){
 		$originUrl = urldecode($headers["Origin-Url"]);
-        $html = getHtml($originUrl, $post, $headers);
-        $charset = "utf-8";
-        foreach($http_response_header as $header){
-            if(preg_match('/charset.*?([\w-]+)/', $header, $matches)){
+        $headerArray = array();
+        foreach($headers as $key => $value){
+            if($key=="Host" || $key=="User-Agent")
+                continue;
+            $headerArray[] = $key.":".$value;
+        }
+        $ch = curl_init($url);
+        print_r($headerArray);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_POST, $post);
+        $response = curl_exec($ch);
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $responseHeaders = substr($response, 0, $headerSize);
+        curl_close($ch);
+        
+        $headerArray =  explode("\r\n", $responseHeaders);
+        $charset = "none";
+        foreach($headerArray as $header){
+            if(preg_match('/charset.*?([\w-]+)/i', $header, $matches)){
                 $charset = $matches[1];
             }
         }
-	    header( "Content-Type:text/plain;charset=".$charset);  
+        $charset = ($charset != "none")?";charset=".$charset:"";
+	    header( "Content-Type:text/plain".$charset);  
+        $html = substr($response, $headerSize);
 		echo $html;
 	}else{
 	    header( "Content-Type:text/plain;charset=utf-8");  
@@ -36,15 +56,4 @@
 		
 		return $headers; 
 	} 
-    
-    function getHtml($url, $post = false, $headers = array()){
-        $headerStr = "";
-        foreach($headers as $key => $value){
-            $headerStr .= $key.":".$value."\r\n";
-        }
-        $opts = array('http' => array('header' => $headerStr, 'timeout' => 30), );
-        $context = stream_context_create($opts);
-        $code = file_get_contents($url, false, $context);
-		return $code;
-    }
 ?>
